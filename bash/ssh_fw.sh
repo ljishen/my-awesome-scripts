94f065ca30160e810e8fa9ce7f72523d713c0b1a
Jianshen Liu <jliu120@ucsc.edu>
2018-09-10 03:20:12 +0000

    Add SSH connection forwarding script

---
+++ bash/ssh_fw.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+#
+# MIT License
+#
+# Copyright (c) 2018 Jianshen Liu
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# Run this script on the host that you want to forward the SSH connection
+# (port 22) to from the remote address.
+
+set -eu -o pipefail
+
+script="$(basename "$0")"
+args=( "$@" )
+
+function usage {
+    printf "usage: %s [REMOTE] start|stop
+\tREMOTE:\t[user@]bind_address[:port]
+
+REMOTE is required when start this service.
+" "$script"
+    exit 1
+}
+
+args_len="${#args[@]}"
+if [ "$args_len" -lt 1 ]; then
+    usage
+fi
+
+action="${args[-1]}"
+if [ "$action" != "start" ] && [ "$action" != "stop" ]; then
+    usage
+fi
+
+PID_FILE="$HOME"/.ssh_fw_pid
+
+if [ "$action" == "stop" ]; then
+    if [ -f "$PID_FILE" ]; then
+        pid="$(cat "$PID_FILE")"
+        if kill -SIGKILL "$pid" &> /dev/null; then
+            echo "PID $pid has stopped."
+        fi
+        rm -f "$PID_FILE"
+    fi
+else
+    # action start
+
+    if [ "$args_len" -ne 2 ]; then
+        usage
+    fi
+
+    bind_address="${args[0]}"
+    port=8111
+    if [[ $bind_address == *:* ]]; then
+        port="$(grep -oP ':\K\d+' <<< "$bind_address")"
+        bind_address="${bind_address%:*}"
+    fi
+
+    # privileged port requires root so it is not allowed.
+    # https://www.w3.org/Daemon/User/Installation/PrivilegedPorts.html
+    # https://linux.die.net/man/1/ssh
+    if [ "$port" -lt 1024 ]; then
+        echo "Not allow to forward to privileged port (1-1023)"
+        exit 1
+    fi
+
+    log_file="$(pwd)/ssh_fw.log"
+    nohup ssh -o PasswordAuthentication=no -N "$bind_address" -R "$port":localhost:22 > "$log_file" & echo $! > "$PID_FILE"
+    sleep 3
+    pid="$(cat "$PID_FILE")"
+    if ! ps -p "$pid" > /dev/null; then
+        cat "$log_file"
+        exit 1
+    fi
+
+    echo "Remote SSH port forwarding (remote $port -> local 22) is running at PID $pid"
+fi
+
+echo "Command completed succeeded."
