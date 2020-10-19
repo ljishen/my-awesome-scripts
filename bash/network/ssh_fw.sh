cf9eea9c4f02a524cc0ee624dc35707c7973ef8f
Jianshen Liu <jliu120@ucsc.edu>
2020-10-19 15:32:04 -0700

    Enable ssh_fw.sh to connect with a specified identity file

--- bash/network/ssh_fw.sh
+++ bash/network/ssh_fw.sh
@@ -27,42 +27,53 @@
 
 set -eu -o pipefail
 
-script="$(basename "$0")"
+script_name="$(basename "$0")"
 args=("$@")
 
 function usage() {
-  printf "usage: %s <REMOTE> start|stop
+  printf "usage: %s [REMOTE_HOST] [SSH_OPTIONS] [stop]
 
-REMOTE:\\t[user@]bind_address[:port]
+REMOTE_HOST: [user@]bind_address[:port]
 
-" "$script"
+Examples:
+  # start forwarding with a specified identity file
+  ./%s user@piha.soe.ucsc.edu:8111 -i \"$HOME/.ssh/id_rsa\"
+
+  # stop forwarding by killing the forwarding ssh process
+  ./%s stop
+
+" "$script_name" "$script_name" "$script_name"
   exit 1
 }
 
 args_len="${#args[@]}"
-if [ "$args_len" -lt 1 ]; then
+if [[ "$args_len" -lt 1 ]]; then
+  # case with one parameter: ssh_fw.sh stop
   usage
 fi
 
+pid_file="$HOME/.${script_name%.*}_pid"
 action="${args[-1]}"
-if [ "$action" != "start" ] && [ "$action" != "stop" ]; then
-  usage
-fi
 
-PID_FILE="$HOME"/.ssh_fw_pid
-
-if [ "$action" == "stop" ]; then
-  if [ -f "$PID_FILE" ]; then
-    pid="$(cat "$PID_FILE")"
-    if kill -SIGKILL "$pid" &> /dev/null; then
-      echo "PID $pid has stopped."
+if [[ "$action" == "stop" ]]; then
+  if [[ -f "$pid_file" ]]; then
+    exit_status=0
+    pkill --signal SIGTERM --pidfile "$pid_file" || exit_status=$?
+    if [[ "$exit_status" -eq 0 ]]; then
+      echo "Terminated PID $(cat "$pid_file")"
+      rm -f "$pid_file"
+    else
+      echo "Fail to terminate PID $(cat "$pid_file")"
+      exit "$exit_status"
     fi
-    rm -f "$PID_FILE"
+  else
+    echo "pidfile $pid_file does not exist!"
+    exit 2
   fi
 else
   # action start
 
-  if [ "$args_len" -ne 2 ]; then
+  if [[ "$args_len" -lt 1 ]]; then
     usage
   fi
 
@@ -76,34 +87,34 @@ else
   # port number less than 1024 requires root privilege.
   # https://www.w3.org/Daemon/User/Installation/PrivilegedPorts.html
   # https://linux.die.net/man/1/ssh
-  if [ "$port" -lt 1024 ]; then
-    if [ "$EUID" -ne 0 ]; then
+  if [[ "$port" -lt 1024 ]]; then
+    if [[ "$EUID" -ne 0 ]]; then
       printf "Forwarding to privileged port (1-1023) requires sudo.\\n\\n"
-      exit 1
+      exit 3
     fi
 
     echo "WARNING: forwarding to privileged port is not guaranteed to work."
   fi
 
-  log_file="/tmp/ssh_fw.log"
-  nohup ssh \
+  log_file="/tmp/${script_name%.*}.log"
+  stdbuf -oL nohup ssh \
     -o PasswordAuthentication=no \
     -o ExitOnForwardFailure=yes \
     -o ServerAliveInterval=60 \
     -N "$bind_address" \
     -R "$port":localhost:22 \
+    "${args[@]:1}" \
     < /dev/null \
-    > "$log_file" 2>&1 &
-  echo $! > "$PID_FILE"
+    >"$log_file" 2>&1 &
+  echo $! >"$pid_file"
   sleep 3
-  if ! pgrep --pidfile "$PID_FILE" > /dev/null; then
+  if ! pgrep --pidfile "$pid_file" >/dev/null; then
     cat "$log_file"
     echo
-    exit 1
+    wait "$(cat "$pid_file")"
+    exit $!
   fi
 
   printf "Remote SSH port forwarding (remote %d -> local 22) is running (PID %d).\\n" \
-    "$port" "$(cat "$PID_FILE")"
+    "$port" "$(cat "$pid_file")"
 fi
-
-printf "Command has completed successfully.\\n\\n"
